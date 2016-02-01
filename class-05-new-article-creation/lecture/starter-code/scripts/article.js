d59933874cce1e344bb668f59a8129e89874d07c
Dan Delp <delpdk@gmail.com>
2016-01-31 16:12:30 -0800

    Modified file while practicing

--- class-05-new-article-creation/lecture/starter-code/scripts/article.js
+++ class-05-new-article-creation/lecture/starter-code/scripts/article.js
@@ -10,12 +10,12 @@ function Article (opts) {
 }
 
 Article.prototype.toHtml = function() {
-  var template = Handlebars.compile($('#article-template').text());
+  var template = Handlebars.compile($('.template').html());
 
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
   this.body = marked(this.body);
-  
+
   return template(this);
 };
 
